d7d512c130b253e3a5a36a46ae483b81c78b35c6
Kaan <kaanhas.52@gmail.com>
2014-07-14 15:23:45 +0300

    upload project

---
+++ TestCSharp/TestCSharp/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestCSharp
+{
+    public partial class Form1 : Form
+    {
+        string path;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            path = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string newName = path + "\\" + textBox1.Text;
+
+                if (!Directory.Exists(newName))  // if it doesn't exist, create
+                    Directory.CreateDirectory(newName);
+            }
+            catch (System.IO.IOException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
